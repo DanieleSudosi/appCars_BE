7b1b5017fe4e4547d81db03ce74e73fe110cd3b7
DanieleSudosi <daniele.sudosi@gmail.com>
2022-07-19 14:53:11 +0200

    patch worki

--- src/main/java/com/finance/appCars/repository/NoleggiatoreRepository.java
+++ src/main/java/com/finance/appCars/repository/NoleggiatoreRepository.java
@@ -1,6 +1,7 @@
 package com.finance.appCars.repository;
 
 import com.finance.appCars.domain.Noleggiatore;
+import com.finance.appCars.domain.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoleggiatoreRepository extends JpaRepository<Noleggiatore,Long>, JpaSpecificationExecutor<Noleggiatore> {
+    Noleggiatore findByUtente(Utente utente);
 }
